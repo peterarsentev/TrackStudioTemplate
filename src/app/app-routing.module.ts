47be70a8ec4a0c63427bd3d9eeeb587e92fd41da
Andrei Hincu <a.hincu@octopod.ru>
2020-02-11 21:15:05 +0200

    add routing,nav bar, main layout , login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { LoginComponent } from './shared/components/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainLayoutComponent, children: [
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      {path: 'login', component: LoginComponent}
+    ] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { LoginComponent } from './shared/components/login/login.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
