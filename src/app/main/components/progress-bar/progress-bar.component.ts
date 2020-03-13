e666ff3666782fcc8478420250fd850c962ee5b7
Andrei Hincu <a.hincu@octopod.ru>
2020-03-13 09:42:52 +0200

    add unsubscribe in status bar

--- src/app/main/components/progress-bar/progress-bar.component.ts
+++ src/app/main/components/progress-bar/progress-bar.component.ts
@@ -1,18 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { TasksService } from '../../../shared/services/tasks.service';
 import { TaskModel } from '../../../shared/models/task.model';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.html',
   styleUrls: ['./progress-bar.component.scss']
 })
-export class ProgressBarComponent implements OnInit {
+export class ProgressBarComponent implements OnInit, OnDestroy {
 
-  @Input()  task: TaskModel = {};
+  @Input() task: TaskModel = {};
   allTasksCount  = 1;
   solvedTasksCount: number;
   barValue = 0;
+  private ngUnsubscribe$: Subject<void> = new Subject<void>();
 
   constructor(private tasksService: TasksService) { }
 
@@ -23,6 +26,7 @@ export class ProgressBarComponent implements OnInit {
 
   getAllTasks() {
     this.tasksService.getTaskCount(this.task.id, true)
+      .pipe(takeUntil(this.ngUnsubscribe$))
       .subscribe(res => {
         this.allTasksCount = res.total;
       })
@@ -30,9 +34,16 @@ export class ProgressBarComponent implements OnInit {
 
   getSolvedTasks() {
     this.tasksService.getTaskCount(this.task.id, false)
+      .pipe(takeUntil(this.ngUnsubscribe$))
       .subscribe(res => {
         this.solvedTasksCount = res.total;
         this.barValue = Math.round(this.solvedTasksCount / this.allTasksCount) * 100;
       })
   }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe$.next();
+    this.ngUnsubscribe$.complete();
+  }
+
 }
