b7394d5f42d3d1e7d1400613b383bc9d6603ac88
Andrei Hincu <a.hincu@octopod.ru>
2020-04-21 09:59:51 +0300

    добавил роут в случае 404 переход на страницу тех обслуживания

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { InterseptorService } from './shared/services/interseptor.service';
+import { PreventionComponent } from './prevention/prevention.component';
 
 const INTERCEPTOR_PROVIDER: Provider = {
   provide: HTTP_INTERCEPTORS,
@@ -17,6 +18,7 @@ const INTERCEPTOR_PROVIDER: Provider = {
 @NgModule({
   declarations: [
     AppComponent,
+    PreventionComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/shared/services/interseptor.service.ts
+++ src/app/shared/services/interseptor.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { AuthService } from './auth.service';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({providedIn: 'root'})
 export class InterseptorService implements HttpInterceptor {
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    return next.handle(req);
+    return next.handle(req)
+      .pipe(
+        catchError((err: HttpErrorResponse) => {
+          if (err.status === 404) {
+            this.router.navigate(['/prevention']);
+          }
+          return throwError(err);
+        })
+      );
   }
 
 }
