aa9ad2444a460de0bc2306a47bea1bd71d7efcb1
Andrei Hincu <a.hincu@octopod.ru>
2020-05-07 19:48:47 +0300

    добавил ответственного по умолчанию при создании задачи

--- src/app/main/components/new-task/new-task.component.ts
+++ src/app/main/components/new-task/new-task.component.ts
@@ -33,8 +33,8 @@ export class NewTaskComponent implements OnInit, OnDestroy {
               private router: Router) { }
 
   ngOnInit() {
-    this.getRoutParams();
     this.initForm();
+    this.getRoutParams();
   }
 
   private getRoutParams() {
@@ -47,6 +47,7 @@ export class NewTaskComponent implements OnInit, OnDestroy {
       takeUntil(this.ngUnsubscribe$)
     ).subscribe(handlers => {
       this.handlers = handlers.handlers;
+      this.handlers.forEach(user => user.name === 'Петр Арсентьев' ?  this.form.get('handlerId').setValue(user.id) : null)
     });
   }
 
