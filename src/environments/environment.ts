21bfda0538c066a54bf78e30c1360f6db37abbee
andrei.hincu <a.hincu@octopod.ru>
2021-04-04 20:08:30 +0300

    поправил урл

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import { Environment } from './interface';
 
 export const environment: Environment = {
   production: false,
-   urlJedu: 'http://localhost:9090',
+  // urlJedu: 'http://localhost:9090',
+     urlJedu: 'https://job4j.ru/jedu'
 };
 
 /*
