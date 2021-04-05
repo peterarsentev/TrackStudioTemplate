be4ce56ac5172e1673e80454d061eff76bbe21a1
parsentev <parsentev@yandex.ru>
2021-04-05 16:44:26 +0300

    Заменил адрес сервера.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Environment } from './interface';
 
 export const environment: Environment = {
   production: true,
-  urlJedu: 'https://job4j.ru/jedu'
+  urlJedu: 'https://job4j.ru/api'
 };
