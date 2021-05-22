d181dccceb2e33c72578b126c99c95092195a46c
andrei.hincu <a.hincu@octopod.ru>
2021-05-22 16:18:01 +0300

    Категории и уровнь.[#535310]

--- src/app/shared/models/topic.models.ts
+++ src/app/shared/models/topic.models.ts
@@ -1,7 +1,12 @@
+import { LevelModels } from './level.models';
+import { CategoryModels } from './category.models';
+
 export class TopicModels {
   constructor(
     public id?: string,
-    public name?: string
+    public name?: string,
+    public level?: LevelModels,
+    public category?: CategoryModels
   ) {
   }
 }
